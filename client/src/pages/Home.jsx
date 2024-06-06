05bf681ae79ba01068fa18d75683233a9ea6073d
Truman Ngo <truman2cool@gmail.com>
2024-06-06 19:13:29 -0400

    updated Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ function Home(){
     return(
         <div className = 'Home'>
             <h1>Welcome to ShabuGram</h1>
+            <img src="assets/Shabu.png" alt="Default img "/>
         </div>
     )
 }
